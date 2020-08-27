1e40ed4c14ba6e4ab66fef0a3324380faab41f65
Kerem Çelebi <kerem.celebi@inveon.com.tr>
2020-08-27 17:17:30 +0300

    Components created.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpenses } from './components/IncomeExpenses'
+import { TransactionList } from './components/TransactionList'
+import { AddTransaction } from './components/AddTransaction'
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header></Header>
+      <div className="container">
+        <Balance></Balance>
+        <IncomeExpenses></IncomeExpenses>
+        <TransactionList></TransactionList>
+        <AddTransaction></AddTransaction>
+      </div>
     </div>
   );
 }
---
+++ src/components/AddTransaction.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export const AddTransaction = () => {
+
+    const [text, setText] = useState("");
+    const [amount, setAmount] = useState(0);
+
+    return (
+        <div>
+            <h3>Add new transaction</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="text"></label>
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..."></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount">
+                        Amount <br />
+                        (negative - expense, positive - income)
+                    </label>
+                    <input type="number" value={text} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..."></input>
+                </div>
+            </form>
+        </div>
+    )
+}
